d99df7e179469d8259a4bb7a5d68bd41398dfe2f
晓荷 <xhlsrj@qq.com>
2024-01-25 15:03:17 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { NModal } from 'naive-ui';
+import { ref } from 'vue';
+import OneBox from './OneBox.vue';
+
+const isShowModal = ref(false);
+const value = ref('');
+function fn() {}
+</script>
+
+<template>
+  <div class="click" @click="isShowModal = true">弹窗</div>
+  <NModal v-model:show="isShowModal" :mask-closable="false">
+    <div>
+      <OneBox>
+        <input v-model="value" />
+        <div>
+          <div style="display: block" @click="fn">fn</div>
+        </div>
+      </OneBox>
+      <div class="click" @click="isShowModal = false">关闭</div>
+    </div>
+  </NModal>
+</template>
+
+<style scoped>
+.click {
+  cursor: pointer;
+  user-select: none;
+}
+</style>
